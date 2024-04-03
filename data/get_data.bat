4ec3f7808ac07f6ed7f3a7428ac9c2e48b7dcf85
Nick Fox-Gieg <nick@fox-gieg.com>
2024-04-03 14:02:26 -0400

    Update get_data.bat

--- data/get_data.bat
+++ data/get_data.bat
@@ -3,6 +3,8 @@
 cd %~dp0
 powershell -Command "Invoke-WebRequest https://fox-gieg.com/patches/github/n1ckfg/segment-anything/data/data.zip -OutFile data.zip"
 powershell Expand-Archive data.zip -DestinationPath .
+mv .\data\* .
+rmdir data
 del data.zip
 
 @pause
