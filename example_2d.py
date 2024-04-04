a964d7559619bfa994244ce6f4eed54164169f93
Nick Fox-Gieg <nick@fox-gieg.com>
2024-04-03 23:46:56 -0400

    Update example_2d.py

--- example_2d.py
+++ example_2d.py
@@ -5,7 +5,7 @@ import sam_wrapper as sw
 
 mask_generator = sw.setup()
 
-image_bgr = cv2.imread("data/biscarosse.jpg")
+image_bgr = cv2.imread("data/biscarosse_c.jpg")
 image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 result = mask_generator.generate(image_rgb)
 
